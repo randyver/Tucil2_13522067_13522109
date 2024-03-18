53ca5d06e2b7d42cea848857ec9e47e7c4075ca8
randyver <randyverdian99@gmail.com>
2024-03-18 23:14:04 +0700

    feat: execution time

--- src/bezier_visualize.py
+++ src/bezier_visualize.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import bezier_curve
+import time
 
 
 def visualize(order: int, control_points: list[tuple[float, float]], iteration: int):   
@@ -9,7 +10,14 @@ def visualize(order: int, control_points: list[tuple[float, float]], iteration: 
     for point in control_points:
         plt.scatter(point[0], point[1], s=100, color="orange")
 
+    start_time = time.time()
+
     result = bezier_curve.divide_and_conquer(order, control_points, iteration)
+
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Waktu eksekusi Divide and Conquer: {execution_time} detik")
+
     for point in result:
         plt.scatter(point[0], point[1], s=30, color="red")
 
@@ -24,7 +32,14 @@ def visualize_bruteforce(order: int, control_points: list[tuple[float, float]]):
     for point in control_points:
         plt.scatter(point[0], point[1], s=100, color="orange")
 
+    start_time = time.time()
+
     result = bezier_curve.bruteforce_general(order, control_points)
+
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Waktu eksekusi Bruteforce: {execution_time} detik")
+
     for point in result:
         plt.scatter(point[0], point[1], s=5, color="red")
     
